d83fcac29e82b49cb669dcc25a65b62f5a7f8c10
PhamDinhMinh <phamdinhminh030801@gmail.com>
2024-05-01 22:14:58 +0700

    fix: update user

--- Do_An_Tot_Nghiep/Dto/User/UserUpdateDto.cs
+++ Do_An_Tot_Nghiep/Dto/User/UserUpdateDto.cs
@@ -4,8 +4,6 @@ public class UserUpdateDto
 {
     public string? Gender { get; set; }
     public string? EmailAddress { get; set; }
-    public string? ImageUrl { get; set; }
-    public string? CoverImageUrl { get; set; }
     public string? Name { get; set; }
     public string? PhoneNumber { get; set; }
     public DateTime? DateOfBirth { get; set; }
